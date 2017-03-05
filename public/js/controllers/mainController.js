ad1009a6aaa38d9f5ae3d0298a2706b00190553b
alex <alexbnk@gmail.com>
2017-03-05 10:38:09 +0200

    added listBeersCtrl that put sthe beersObj in the scope, inserted 8 beers in the factory, copy of same Corona

---
+++ public/js/controllers/mainController.js
@@ -0,0 +1,12 @@
+app.controller('listBeersCtrl', function($scope,beersFactory){
+
+$scope.beers=beersFactory.beersObj;
+//$scope.buyMovie = moviesFactory.buyMovie;
+// $scope.change = todoFactory.change;
+// $scope.delete = todoFactory.delete;
+// $scope.add = todoFactory.add;
+// $scope.done = todoFactory.done;
+
+// closes controller
+
+});
--- public/js/services/beers.js
+++ public/js/services/beers.js
@@ -2,14 +2,60 @@ app.factory('beersFactory', function () {
 
 var beers = [
   {
-    "name":"Corona",
+    "name":"Corona 1",
     "style": "light",
     "abv": 5.9,
     "image_url":"/img/Corona-Beer.jpg"
+},
+{
+  "name":"Corona 2",
+  "style": "light",
+  "abv": 5.9,
+  "image_url":"/img/Corona-Beer.jpg"
+},
+{
+  "name":"Corona 3",
+  "style": "light",
+  "abv": 5.9,
+  "image_url":"/img/Corona-Beer.jpg"
+},
+{
+  "name":"Corona 4",
+  "style": "light",
+  "abv": 5.9,
+  "image_url":"/img/Corona-Beer.jpg"
+},
+{
+  "name":"Corona 5",
+  "style": "light",
+  "abv": 5.9,
+  "image_url":"/img/Corona-Beer.jpg"
+},
+{
+  "name":"Corona 6",
+  "style": "light",
+  "abv": 5.9,
+  "image_url":"/img/Corona-Beer.jpg"
+},
+{
+  "name":"Corona 7",
+  "style": "light",
+  "abv": 5.9,
+  "image_url":"/img/Corona-Beer.jpg"
+},
+{
+  "name":"Corona 8",
+  "style": "light",
+  "abv": 5.9,
+  "image_url":"/img/Corona-Beer.jpg"
 }
   ];
 
-return { beers:beers
-  };
+  var beersObj = {
+    beers:beers
+    };
+
+
+return beersObj;
 
 });
