5229370880123002168072eff723bfd82166941f
alex <alexbnk@gmail.com>
2017-03-09 13:25:16 +0200

    Finished Server API

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -34,8 +34,44 @@ $scope.addBeer = function() {
 
 app.controller('beersListCtrl', function($scope,beersFactory){
 $scope.beers = beersFactory.beers;
+$scope.doOrder = false;
+$scope.sort  = function() {
+  if ($scope.doOrder==true) {
+    $scope.doOrder=false;
+  } else {
+    $scope.doOrder=true;
+  }
+};
+
+$scope.rateThis =beersFactory.rateThis;
+// $scope.rateThis = function (id,rate) {
+//   console.log("Beer # " +id + " was rated : "+rate+" stars");
+//   beersFactory.beers[id].ratings.push(rate);
+//   $scope.rating(id);
+//   console.log(beersFactory.beers);
+//   };
+
 $scope.removeBeer = function (id) {
   console.log("removeBeer called in controller!! with this id:"+ id);
   beersFactory.removeBeer(id);
+
 };
+
+// $scope.rating = function(id) {
+//   var nRatings = beersFactory.beers[id].ratings.length;
+//   var rValue =0;
+//   var ratingValue =0;
+//   console.log(nRatings + " ratings found for beer "+id+" :")
+//    for (var i=0; i<nRatings ; i++) {
+//     rValue= rValue+beersFactory.beers[id].ratings[i];
+//     console.log(beersFactory.beers[id].ratings[i]);
+//    //console.log(nRatings);
+//  };
+$scope.rating = beersFactory.rating;
+
+
+
+
+   //  set rating in object
+
 });
